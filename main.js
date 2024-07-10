c7f3847fe84a4e85575fcdc0411abc9c41a555fe
zah-ara <161930119+zah-ara@users.noreply.github.com>
2024-07-10 14:17:07 +0500

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-import inquirer from "inquirer";
+#! /usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
 let todos = [];
 let condition = true;
 while (condition) {
-    let addTask = await inquirer.prompt([
+    let addTask = await inquirer_1.default.prompt([
         {
             name: "todo",
             type: "input",
